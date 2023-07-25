ab58956cd5085dc2fb1d1e869ed2fd521bd41b8a
Seulah Kim <seulahkim@chunjae.co.kr>
2023-07-25 13:35:24 +0900

    배포

---
+++ src/db/mongo.ts
@@ -0,0 +1,51 @@
+// mongo.ts
+import { MongoClient } from 'mongodb';
+import { MONGO_URL } from '../lib/Env';
+
+import type { returnURLsList } from './BackendUtils';
+
+ 
+console.log('url >>> ' + MONGO_URL);
+const client = new MongoClient(MONGO_URL);
+
+ 
+export function start_mongo(): Promise<MongoClient> {
+	console.log('Starting mongo...');
+	return client.connect();
+}
+ 
+export default client.db('mydb');
+
+// export async function load({}) {
+// 	const client = new MongoClient(MONGO_URL);
+// 	let dbError = {
+// 		hasError: false,
+// 		error: '',
+	
+// 	}
+// 	let URLList:string[] = [];
+
+// 	try {
+// 		await client.connect();
+// 	}
+// 	catch(error: any){
+// 		dbError.hasError = true;
+// 		dbError.error = error.message?? "Error Connection to DB"
+// 	}
+
+// 	const dbName = 'mydb';
+// 	const db = client.db(dbName);
+// 	const collName = 'tutorials'
+// 	const collection = db.collection(collName);
+
+// 	try{
+// 		URLList = await returnURLsList(collection);
+// 		console.log("URL List")
+// 	} catch (error: any){
+// 		dbError.hasError = true;
+// 		dbError.error = error.message?? "Error retrieving URL List";
+// 	}
+
+// 	return {dbError, URLList}
+	
+// }
